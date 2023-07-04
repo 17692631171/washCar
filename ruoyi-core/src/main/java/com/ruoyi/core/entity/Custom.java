91abb1f753f7f5959692837ee4ad119f3f6c9e5b
zhangyu <appid@tom.com>
2023-07-04 15:14:04 +0800

    Changes

--- ruoyi-core/src/main/java/com/ruoyi/core/entity/Custom.java
+++ ruoyi-core/src/main/java/com/ruoyi/core/entity/Custom.java
@@ -1,17 +1,27 @@
 package com.ruoyi.core.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.ruoyi.common.xss.Xss;
+
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
 /**
- * 顾客基础表
+ * 顾客信息表
+ * @author 张玉
  * @TableName t_custom
  */
+@TableName(value ="t_custom")
 public class Custom implements Serializable {
     /**
-     * 顾客ID
+     * 顾客id
      */
-    private String customId;
+    @TableId
+    private Long customId;
 
     /**
      * 真名
@@ -24,35 +34,19 @@ public class Custom implements Serializable {
     private String nickName;
 
     /**
-     * 账户名
-     */
-    private String userName;
-
-    /**
-     * 密码
-     */
-    private String passWord;
-
-    /**
      * 地址
      */
     private String address;
 
-
     /**
-     * 生日
+     * 注册类型 0小程序，1手动
      */
-    private Date birthdayTime;
-
-    /**
-     * 注册方式
-     */
-    private String regType;
+    private Integer regType;
 
     /**
      * 职业
      */
-    private String job;
+    private Integer job;
 
     /**
      * 注册时间
@@ -60,33 +54,34 @@ public class Custom implements Serializable {
     private Date regTime;
 
     /**
-     * 最后一次更新时间
+     * 修改时间
      */
     private Date updateTime;
 
     /**
-     * 状态
+     * 状态 0正常，1封存，2停用
      */
-    private String status;
+    private Integer status;
 
     /**
      * 备注
      */
-    private String remarks;
+    private String remakes;
 
+    @TableField(exist = false)
     private static final long serialVersionUID = 1L;
 
     /**
-     * 顾客ID
+     * 顾客id
      */
-    public String getCustomId() {
+    public Long getCustomId() {
         return customId;
     }
 
     /**
-     * 顾客ID
+     * 顾客id
      */
-    public void setCustomId(String customId) {
+    public void setCustomId(Long customId) {
         this.customId = customId;
     }
 
@@ -119,34 +114,6 @@ public class Custom implements Serializable {
     }
 
     /**
-     * 账户名
-     */
-    public String getUserName() {
-        return userName;
-    }
-
-    /**
-     * 账户名
-     */
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    /**
-     * 密码
-     */
-    public String getPassWord() {
-        return passWord;
-    }
-
-    /**
-     * 密码
-     */
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
-
-    /**
      * 地址
      */
     public String getAddress() {
@@ -160,46 +127,31 @@ public class Custom implements Serializable {
         this.address = address;
     }
 
-
     /**
-     * 生日
+     * 注册类型 0小程序，1手动
      */
-    public Date getBirthdayTime() {
-        return birthdayTime;
-    }
-
-    /**
-     * 生日
-     */
-    public void setBirthdayTime(Date birthdayTime) {
-        this.birthdayTime = birthdayTime;
-    }
-
-    /**
-     * 注册方式
-     */
-    public String getRegType() {
+    public Integer getRegType() {
         return regType;
     }
 
     /**
-     * 注册方式
+     * 注册类型 0小程序，1手动
      */
-    public void setRegType(String regType) {
+    public void setRegType(Integer regType) {
         this.regType = regType;
     }
 
     /**
      * 职业
      */
-    public String getJob() {
+    public Integer getJob() {
         return job;
     }
 
     /**
      * 职业
      */
-    public void setJob(String job) {
+    public void setJob(Integer job) {
         this.job = job;
     }
 
@@ -218,69 +170,60 @@ public class Custom implements Serializable {
     }
 
     /**
-     * 最后一次更新时间
+     * 修改时间
      */
     public Date getUpdateTime() {
         return updateTime;
     }
 
     /**
-     * 最后一次更新时间
+     * 修改时间
      */
     public void setUpdateTime(Date updateTime) {
         this.updateTime = updateTime;
     }
 
     /**
-     * 状态
+     * 状态 0正常，1封存，2停用
      */
-    public String getStatus() {
+    public Integer getStatus() {
         return status;
     }
 
     /**
-     * 状态
+     * 状态 0正常，1封存，2停用
      */
-    public void setStatus(String status) {
+    public void setStatus(Integer status) {
         this.status = status;
     }
 
     /**
      * 备注
      */
-    public String getRemarks() {
-        return remarks;
+    public String getRemakes() {
+        return remakes;
     }
 
     /**
      * 备注
      */
-    public void setRemarks(String remarks) {
-        this.remarks = remarks;
+    public void setRemakes(String remakes) {
+        this.remakes = remakes;
     }
-
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", customId=").append(customId);
-        sb.append(", realName=").append(realName);
-        sb.append(", nickName=").append(nickName);
-        sb.append(", userName=").append(userName);
-        sb.append(", passWord=").append(passWord);
-        sb.append(", address=").append(address);
-        sb.append(", birthdayTime=").append(birthdayTime);
-        sb.append(", regType=").append(regType);
-        sb.append(", job=").append(job);
-        sb.append(", regTime=").append(regTime);
-        sb.append(", updateTime=").append(updateTime);
-        sb.append(", status=").append(status);
-        sb.append(", remarks=").append(remarks);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
+        return "Custom{" +
+                "customId=" + customId +
+                ", realName='" + realName + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", address='" + address + '\'' +
+                ", regType=" + regType +
+                ", job=" + job +
+                ", regTime=" + regTime +
+                ", updateTime=" + updateTime +
+                ", status=" + status +
+                ", remakes='" + remakes + '\'' +
+                '}';
     }
 }
